36ceee34e93c8a0d465483ec425cb0cb0a292f86
JUAN ELIAS ELIAS <1719110241@utectulancingo.edu.mx>
2020-05-28 00:57:48 +0000

    carpeta

---
+++ Semana_2/persona.py
@@ -0,0 +1,11 @@
+Class Persona :
+define (/self nombre, altura, Edad):
+self nombre = nombre Self Oltursa = Itura self. Edad = Edad
+Cklass Hombre (Persona):
+def Hom (sclF):
+print (" Como estas ...
+Class Manager (Persona):
+def Mai Self) :
+print
+como estas amigo
+Person = Hombre ("Person", "1.48, "20 años") Print (Person. nAtura) Person bark ()
